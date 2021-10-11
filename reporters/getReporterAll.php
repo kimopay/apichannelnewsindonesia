18597a5d0953dfda662cd02b8f25984fde8f05b6
it kimopay <it.kimopay@gmail.com>
2021-10-11 16:27:39 +0800

    fdsfsd

---
+++ reporters/getReporterAll.php
@@ -0,0 +1,17 @@
+<?php
+ require_once '../connection.php';
+ header('Content-type: application/json');
+
+$query = "SELECT * FROM reporters ORDER BY id DESC";
+
+ $result = mysqli_query($conn, $query);
+
+ $array = array();
+ while($row = mysqli_fetch_assoc($result)){
+    $array[] = $row;
+ }
+
+ echo ($result) ?
+ json_encode(array("kode" => "1", "reporters_result" => $array)) :
+ json_encode(array("kode" => "0", "pesan" => "Data tidak ditemukan"));
+?>
