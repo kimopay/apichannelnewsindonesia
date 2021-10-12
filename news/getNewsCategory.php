a7c12801ef1487ad855177381ee2f9544f32e288
it kimopay <it.kimopay@gmail.com>
2021-10-12 09:46:09 +0800

    dfgdfhdf

---
+++ news/getNewsCategory.php
@@ -0,0 +1,18 @@
+<?php
+ require_once '../connection.php';
+ header('Content-type: application/json');
+
+$category_id = $_GET["category_id"];
+$query = "SELECT * FROM news WHERE category_id = '$category_id'";
+
+ $result = mysqli_query($conn, $query);
+
+//  $array = array();
+while($row = mysqli_fetch_assoc($result)){
+    $array = $row;
+}
+
+ echo ($result) ?
+ json_encode(array("kode" => "1", "news_item_category" => $array)) :
+ json_encode(array("kode" => "0", "pesan" => "Data tidak ditemukan"));
+?>
